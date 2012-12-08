a9c707d9de0013a3535e2df33a4b04c388e47e91
k3rb3ros <no_reply@k3rb3ros.com>
2012-12-08 12:45:39 -0700

    Trying to get rsa keygen to work

--- down_low/test_crypt.cpp
+++ down_low/test_crypt.cpp
@@ -4,7 +4,10 @@ using namespace std;
 void test_crypt()
 {
 	unsigned char entropy [1024];
+	//unsigned long int size = 0;
 	int err = 0;
+	rsa_key pub_key;
+	//rsa_key priv_key;
 	prng_state random_gen;
 
 	for(int i=0; i<1024; i++) entropy[i] = 0; //init entropy
@@ -14,16 +17,21 @@ void test_crypt()
 		cout << "start error " << error_to_string(err) << endl;
 	}
 	
-	if((err = sprng_add_entropy((unsigned const char*)"Hurpa Derp", 10 , &random_gen)) != CRYPT_OK)
-	{
-		cout << "Add_entropy error " << error_to_string(err) << endl;	
-	}
+	//if((err = sprng_add_entropy((unsigned const char*)"Hurpa Derp", 10 , &random_gen)) != CRYPT_OK)
+	//{
+	//	cout << "Add_entropy error " << error_to_string(err) << endl;	
+	//}
 	
 	if((err = sprng_ready(&random_gen)) != CRYPT_OK)
 	{
 		cout << "Ready error " << error_to_string(err) << endl;
 	}
 
-	sprng_read(entropy, 100, &random_gen);
-	cout << "Entropy {" << entropy << "}\n";
+	//sprng_read(entropy, size, &random_gen);
+	
+	if((err = rsa_make_key(&random_gen, 0, 1024, 65537, &pub_key)) != CRYPT_OK) //fcn is not working
+	{
+		cout << "RSA Key Generation error " << error_to_string(err) << endl;
+	}
+	sprng_done(&random_gen);
 }
