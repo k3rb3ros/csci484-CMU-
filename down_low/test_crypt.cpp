8b4609b7fa17f4d433cdc374777a56823357283f
k3rb3ros <no_reply@k3rb3ros.com>
2012-12-10 09:31:17 -0700

    Got rsa export working I think

--- down_low/test_crypt.cpp
+++ down_low/test_crypt.cpp
@@ -7,17 +7,24 @@ void print_buff(unsigned char* buff, unsigned long int len)
 	for(unsigned long int i = 0; i < len; i++) cout << buff[i];
 }
 
+void init_buff(unsigned char* buff, unsigned long int len)
+{
+	for(unsigned long int i = 0; i < len; i++) buff[i] = 0;
+}
 void test_crypt()
 {
-	#define keysize 2048
+	#define KEYSIZE 2048
 	int err = 0;
 	int rng_idx = -1; //rng index
-	int hash_idx = -1; //hasd index
+	int hash_idx = -1; //hash index
+	int res = -1; //used to check the validity of data
 	ltc_mp = tfm_desc; //register tomsfast math
-	rsa_key key_pair; //rsa key pair
+	rsa_key me; //rsa key pair
+	rsa_key them;
 	prng_state sprng; //prng descriptor
-	unsigned char m1[32], m2[32], out[keysize];
-	unsigned long int s_len = -1;
+	unsigned char m1[32], m2[32], out[KEYSIZE];
+	unsigned long int l1 = -1;
+	unsigned long int l2 = -1;
 	
 	if(register_prng(&sprng_desc) == -1) //register prng
 	{
@@ -42,48 +49,124 @@ void test_crypt()
 		cout << "Ready error " << error_to_string(err) << endl;
 	}
 
-        
+        //*******************************************************************************************************************************
+	//Diffie helmann key exchangei
+        //*******************************************************************************************************************************
+
+	//my key
 	if((err = rsa_make_key(&sprng,         	//PRNG state  //generate rsa pub/private keypair
 			       rng_idx,        	//PRNG idx
-			       keysize/8,       //Size of key
+			       KEYSIZE/8,       //Size of key
 			       65537,          	//e
-			       &key_pair)      	//RSA key
+			       &me)      	//RSA key
 				) != CRYPT_OK) 	//if conditon test
 	{
 		cout << "RSA Key Generation error " << error_to_string(err) << endl; 
 	}
 	else 
 	{
-		cout << "Private key: " << key_pair.d << endl;
-		cout << "Public key: " << key_pair.e << endl;
+		cout << "My Private key: " << me.d << endl;
+		cout << "My Public key: " << me.e << endl;
 	}
 
-	//Attemping RSA encrypt
-	
-	for (int i = 0; i< keysize; i++) out[i] = 0; //init out buffer
-	s_len = sizeof(out);
-	cout << s_len << endl;
-  	strcpy((char*)m1, "Hey hows it going");
-	cout << "Pre-encrypted M1: " << m1 << endl;
-	if((err = rsa_exptmod(	m1,		//Our message
-				32, 		//is 32 bytes
-				out, 		//Output buffer
-				&s_len, 	//Output length
-				PK_PUBLIC, 	//Encrypt mode
-				&key_pair) 	//Our rsa keypair
-	   )			 != CRYPT_OK)
-
+	//there key
+	if((err = rsa_make_key(&sprng,         	//PRNG state  //generate rsa pub/private keypair
+			       rng_idx,        	//PRNG idx
+			       KEYSIZE/8,       //Size of key
+			       65537,          	//e
+			       &them)      	//RSA key
+				) != CRYPT_OK) 	//if conditon test
 	{
-		cout << "rsa_encrypt key " << error_to_string(err);
+		cout << "RSA Key Generation error " << error_to_string(err) << endl; 
 	}
 	else 
 	{
+		cout << "There Private key: " << them.d << endl;
+		cout << "There Public key: " << them.e << endl;
+	}
+	unsigned char my_export[KEYSIZE];
+	unsigned char there_export[KEYSIZE];
+	init_buff(my_export, KEYSIZE);
+	unsigned long int keylen = -1;
+	keylen = KEYSIZE; //A quick way to standardize keysize
+	if((err = rsa_export(my_export,		//Export buffer --> Send over network
+			     &keylen,		//size of key (BYTES)
+			     PK_PUBLIC,		//Export our public key to create the new shared key
+			     &me)		//My public key
+	   ) != CRYPT_OK)
+	{
+		cout << "Error exporting my key" << endl;
+	}
+	else
+	{
+		cout << "My pub key: [[[";
+		print_buff(my_export, (unsigned long int)KEYSIZE);
+		cout << "]]] length of key " <<strlen((const char*)my_export) <<endl;
+	}
+        //*******************************************************************************************************************************
+	//Diffie helmann key exchange
+        //*******************************************************************************************************************************
+
+
+	//###############################################################################################################################
+	//Working RSA encrypt decrypt
+	//###############################################################################################################################
+	//Attemping RSA encrypt
+	
+	for (int i = 0; i< KEYSIZE; i++) out[i] = 0; //init out buffer
+	l1 = sizeof(out);
+	cout << "Keysize (bits) " << l1 << endl;
+  	strcpy((char*)m1, "Hey hows it going");
+	cout << "Pre-encrypted M1: " << m1 << endl;
+	if((err = rsa_encrypt_key(	m1,		//Our message
+					32, 		//is 32 bytes
+					out, 		//Output buffer
+					&l1, 		//Output length
+					NULL,		//lparam
+					0,		//With a length of zero
+					&sprng, 	//Our rng state
+					rng_idx,	//Our rng index
+					hash_idx,	//Our hash index
+					&me) 	//Our rsa keypair
+	   )			 != CRYPT_OK)
+
+	{
+		cout << "Encryption Error " << error_to_string(err);
+	}
+	else
+	{
 		cout << "Strcmpr test " << strcmp((const char*)out, (const char*)"\0") << endl;
-		cout << "Message sucessfully rsa encrypted {";
-		print_buff(out, keysize);
-		cout << "}\n";
+		cout << "Message sucessfully rsa encrypted {" << out << "}\n";
 	}
 
-	if(err != CRYPT_OK)rsa_free(&key_pair); //free the key when we are done with it;
+	//Attempting rsa decrypt
+	if((err = rsa_decrypt_key(	out,		//Our message
+					l1, 		//is 32 bytes
+					m2, 		//Output buffer
+					&l2, 		//Output length
+					NULL,		//lparam
+					0,		//With a length of zero
+					hash_idx,	//Our hash index
+					&res,		//Validity of data
+					&me) 	//Our rsa keypair
+	   )			 != CRYPT_OK)
+
+	{
+		cout << "Encryption Error " << error_to_string(err);
+	}
+	else if(res == 1)
+	{
+		cout << "Message sucessfully decrypted {" << m2 << "}\n";
+	}
+	else
+	{
+		cout << "Error message corrupted in transit\n";
+	}
+
+	//###############################################################################################################################
+	//HERE
+	//###############################################################################################################################
+
+	if(err != CRYPT_OK)rsa_free(&me); //free the key when we are done with it;
 	sprng_done(&sprng); //done generating random numbers
 }
