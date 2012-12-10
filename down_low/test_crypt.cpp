b95cdcda1990ffeba5eb71fcf5ab2e9bc52dab19
k3rb3ros <no_reply@k3rb3ros.com>
2012-12-10 16:33:37 -0700

    added experimental import key functions

--- down_low/test_crypt.cpp
+++ down_low/test_crypt.cpp
@@ -69,7 +69,7 @@ void test_crypt()
 		cout << "My Public key: " << me.e << endl;
 	}
 
-	//there key
+	//their key
 	if((err = rsa_make_key(&sprng,         	//PRNG state  //generate rsa pub/private keypair
 			       rng_idx,        	//PRNG idx
 			       KEYSIZE/8,       //Size of key
@@ -85,10 +85,11 @@ void test_crypt()
 		cout << "There Public key: " << them.e << endl;
 	}
 	unsigned char my_export[KEYSIZE];
-	unsigned char there_export[KEYSIZE];
+	unsigned char their_export[KEYSIZE];
 	init_buff(my_export, KEYSIZE);
 	unsigned long int keylen = -1;
 	keylen = KEYSIZE; //A quick way to standardize keysize
+	//Export my key to buffer
 	if((err = rsa_export(my_export,		//Export buffer --> Send over network
 			     &keylen,		//size of key (BYTES)
 			     PK_PUBLIC,		//Export our public key to create the new shared key
@@ -99,10 +100,59 @@ void test_crypt()
 	}
 	else
 	{
+		cout << "My_key succesfully exported\n"; 
+		/*
 		cout << "My pub key: [[[";
 		print_buff(my_export, (unsigned long int)KEYSIZE);
 		cout << "]]] length of key " <<strlen((const char*)my_export) <<endl;
+		*/
 	}
+	
+	if((err = rsa_export(their_export,	//Export buffer --> Send over network
+			     &keylen,		//size of key (BYTES)
+			     PK_PUBLIC,		//Export our public key to create the new shared key
+			     &them)		//My public key
+	   ) != CRYPT_OK)
+	{
+		cout << "Error exporting their key" << endl;
+	}
+	else
+	{
+		cout << "There_key successfully exported\n";
+		/*
+		cout << "My pub key: [[[";
+		print_buff(their_export, (unsigned long int)KEYSIZE);
+		cout << "]]] length of key " <<strlen((const char*)my_export) <<endl;
+		*/
+	}
+	//Export their key to buffer
+	//
+	//Imaginnary Diffie Helman exchange over network takes place 
+	//My_Public Key --> ###Network### --> other person
+	//Me <-- ###Network### <-- Their_Public Key
+	
+	//Mix their key with my key
+	if((err = rsa_import(my_export, KEYSIZE, &them)) != CRYPT_OK)
+	{
+		cout << "Error importing my_public key to them" << endl;
+	}
+	else
+	{
+		cout << "There new Private key: " << them.d << endl;
+		cout << "There new Public key: " << them.e << endl;
+	}
+
+	//Mix my key with their key
+	if((err = rsa_import(their_export, KEYSIZE, &me)) != CRYPT_OK)
+	{
+		cout << "Error importing their_public key to me" << endl;
+	}
+	else
+	{
+		cout << "My new Private key: " << me.d << endl;
+		cout << "My new Public key: " << me.e << endl;
+	}
+
         //*******************************************************************************************************************************
 	//Diffie helmann key exchange
         //*******************************************************************************************************************************
@@ -127,7 +177,7 @@ void test_crypt()
 					&sprng, 	//Our rng state
 					rng_idx,	//Our rng index
 					hash_idx,	//Our hash index
-					&me) 	//Our rsa keypair
+					&them) 	//Our rsa keypair
 	   )			 != CRYPT_OK)
 
 	{
@@ -148,7 +198,7 @@ void test_crypt()
 					0,		//With a length of zero
 					hash_idx,	//Our hash index
 					&res,		//Validity of data
-					&me) 	//Our rsa keypair
+					&them) 	        //There rsa keypair
 	   )			 != CRYPT_OK)
 
 	{
